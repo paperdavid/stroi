cd6d014eb661b8e7458e44b290537c0eb9244a5f
David <paperdavid@gmail.com>
2016-03-06 16:43:36 +0300

    implement files

--- src/js/custom.js
+++ src/js/custom.js
@@ -4,8 +4,47 @@
 
 $(document).ready(function() {
 
-    /* Custom */
+		/* Custom */
 
-    //= ./common/google-analytics.js
+		//= ./common/likely.js
+		//= ./common/owl.carousel.min.js
 
+
+
+	$('.loop').owlCarousel({
+			center: true,
+			items:3,
+			loop:true,
+			margin:10,
+			responsive:{
+						0:{
+								items:1
+						},
+						600:{
+								items:2
+						},
+						1000:{
+								items:3
+						}
+			}
+	});
+
+	$('.about').owlCarousel({
+			center: true,
+			items:3,
+			autoplay:2000,
+			loop:true,
+			margin:10,
+			responsive:{
+						0:{
+								items:1
+						},
+						600:{
+								items:2
+						},
+						1000:{
+								items:3
+						}
+			}
+	});
 });
